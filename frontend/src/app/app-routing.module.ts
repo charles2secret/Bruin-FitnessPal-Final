f6a6ec2757ed724ccaf450e59ea95cbc10924d64
charles2secret <charles2secret@gmail.com>
2020-11-17 17:42:46 +0800

    fix some bugs for login/sign up page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardHomeComponent } from './dashboard-home/dashboard-home.component';
+import { LoginComponent } from './login/login.component';
+import { NavigationPanelComponent } from './navigation-panel/navigation-panel.component';
 
 const routes: Routes = [
-  { path:'home', component: DashboardHomeComponent }
+  { path:'home', component: DashboardHomeComponent },
+  { path:'login', component: LoginComponent},
+  {path:'navigate',component:NavigationPanelComponent}
 ];
 
 @NgModule({
