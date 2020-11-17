2b9cbe2487a427304b60e69fd8861015214218cb
Ao Jiao <aojiao@g.ucla.edu>
2020-11-17 02:13:29 -0800

    a test code for login onInit()

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+//connect to backend
+import { AppService } from "../app.service";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appService: AppService) { }
 
+  /*
   ngOnInit(): void {
   }
+
+   */
+  // a test case for login
+  ngOnInit() {
+    alert("helo?")
+    this.appService.getIssues().subscribe((issue)=>{
+      console.log(issue);
+    })
+
+  }
+
  loginUser(event: any){
    console.log(event)
  }
