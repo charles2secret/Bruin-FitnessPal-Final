a5148f59cc8d5c9e9bbca17b4acb818349054879
Ao Jiao <aojiao@g.ucla.edu>
2020-11-18 22:06:33 -0800

    implementation of login and register is done, tested OK with Postman

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -11,8 +11,41 @@ router.delete('/:_id', deleteUser);
 
 module.exports = router;
 
-function authenticateUser(req, res) { res.send(true)}
-function registerUser(req, res) {res.send(true)}
+/*
+    authenticateUser(req, res)
+    this is a multi-purpose login function
+    can login user both by accountId and username
+    response = successful login will render code 200
+    otherwise, print err message to 400
+    (Bad request means internal error)
+    (other err as string means customized err code)
+ */
+async function authenticateUser(req, res) {
+    let response = await userService.authenticate(req.body);
+    if (response === "successful login") {
+        res.sendStatus(200);
+    }
+    else {
+        res.sendStatus(400).send(response);
+    }
+}
+
+/*
+    registerUser(req, res):
+    send status 200 when registration success
+    otherwise, send 400 with error code: response
+    response follows same format as function authenticate()
+ */
+async function registerUser(req, res) {
+    let response = await userService.create(req.body);
+    if (response === "successful registration") {
+        res.sendStatus(200);
+    }
+    else {
+        res.sendStatus(400).send(response);
+    }
+}
+
 function getCurrentUser(req, res) {res.send(true)}
 function updateUser(req, res) {res.send(true)}
 function deleteUser(req, res) {res.send(true)}
