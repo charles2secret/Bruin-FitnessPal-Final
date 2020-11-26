2dd41d54cf5e6a11c4dd340e2cfd2885595f9328
JamesHYoun <james.hc.youn@gmail.com>
2020-11-26 01:11:02 -0800

    Added Workout Plan component. Started on basic features.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import { FriendsComponent } from './friends/friends.component';
 import { LoginComponent } from './login/login.component';
 import { NavigationPanelComponent } from './navigation-panel/navigation-panel.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { PlanComponent } from './plan/plan.component';
 
 const routes: Routes = [
   { path:'home', component: DashboardHomeComponent },
   { path:'login', component: LoginComponent},
   { path:'navigate', component:NavigationPanelComponent},
   { path:'signUp', component: SignUpComponent},
-  { path:'friends', component: FriendsComponent}
-  
+  { path:'friends', component: FriendsComponent},
+  { path:'plan', component: PlanComponent}
 ];
 
 @NgModule({
